281d61503e846fafbfb7f71ef756304005a4a316
Aryav Khadka <aravxetteri07@gmail.com>
2025-08-05 21:19:19 +0545

    testing

---
+++ test.py
@@ -0,0 +1,51 @@
+a1 =  "hello"
+# # print(a1)
+# print(a1[1])
+# # a1[1] = "c"
+# # print(a1)
+# print(id(a1))
+# print(hex(id(a1)))
+# b =a1.replace("l","o")
+# print(b)
+# for i in b:
+#     print(i)
+
+
+
+# a = 2
+# b = 2
+# c= 2
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print("*"*100)
+
+# l1 = [5,10,"hi", (1,2,3)]
+# for i in l1: print(i)
+# l2 = [[5,10,"hi", (1,2,3)],[6 ,10 ,"hello", (4,5,6)],[7,9,10,"gg",(5,9,0)]]
+# for i in l2: print(i)
+# weather_data = [[10,21,30],[12,34,25],[25,35,34]]
+# temp = []
+# humidity =[]
+# wind = []
+
+# for i in weather_data : 
+#     temp.append(i[0])
+#     humidity.append(i[1])
+#     wind.append(i[2])
+# print(temp)
+# print(humidity)
+# print(wind)
+
+# def avg(list_name):
+#     return sum(list_name)/len(list_name)
+
+# avgwind =avg(wind)
+# print(avgwind)
+# avgtemp =avg(temp)
+# print(avgtemp)
+# avghumidity =avg(humidity)
+# print(avghumidity)
+
+list1 = [55,62,77,88,23,45]
+print(list1[::-1])
